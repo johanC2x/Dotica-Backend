71bbf64c3aee5d1a866c1004b3834220c922f8d4
Johan Cañari <johanc.cca@gmail.com>
2019-06-01 23:06:08 -0500

    ADD CONECT IN PG

--- src/main/java/com/devswpro/controller/IndexController.java
+++ src/main/java/com/devswpro/controller/IndexController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+	@GetMapping("/")
+	public String home() {
+		return "Init DoticaApp";
+	}
+
 	@GetMapping("/inicio")
 	public String index() {
 		return "Hello from DoticaApp";
