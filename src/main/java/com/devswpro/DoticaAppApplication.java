d83a44b54b5ef19edee27f24eab58d9b16360587
Johan Cañari <johanc.cca@gmail.com>
2019-06-09 10:04:56 -0500

    CONFIGURANDO WAR

--- src/main/java/com/devswpro/DoticaAppApplication.java
+++ src/main/java/com/devswpro/DoticaAppApplication.java
@@ -2,12 +2,19 @@ package com.devswpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DoticaAppApplication {
+public class DoticaAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoticaAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DoticaAppApplication.class);
 	}
 
 }
