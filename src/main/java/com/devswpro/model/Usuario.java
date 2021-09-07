cd42760264b27334367e3fd6823178bdd2e3f317
johanc93 <johan.canari@tcs.com>
2021-09-07 15:24:02 -0500

    improvement

--- src/main/java/com/devswpro/model/Usuario.java
+++ src/main/java/com/devswpro/model/Usuario.java
@@ -5,20 +5,20 @@ import java.util.List;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "usuario")
+@Table(name = "usuario", uniqueConstraints={@UniqueConstraint(columnNames = {"nombre"})})
 public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy= GenerationType.AUTO)
 	private Integer idUsuario;
 
-	@Column(name = "nombre", nullable = false, unique = true)
+	@Column(name = "nombre", nullable = false)
 	private String username;
 
 	@Column(name = "nombre_completo", nullable = true)
 	private String nombres;
 
-	@Column(name = "clave", nullable = false, unique = true)
+	@Column(name = "clave", nullable = false)
 	private String password;
 
 	/*
